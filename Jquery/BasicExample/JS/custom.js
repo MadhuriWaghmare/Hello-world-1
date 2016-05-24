f20b0199676fbc02ef2de3090d513ab41503834c
MadhuriWaghmare <madhuri.waghmare@cummins.com>
2016-05-24 17:37:10 +0530

    Add files via upload

---
+++ Jquery/BasicExample/JS/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('div#name').css('color' , 'blue');
+  $('ul').css ('color', 'red');
+  
+   
+   $('div#name').click(function(){
+	    alert("Hello Madhuri  !! Let's Do more practice on jquery");
+	  });
+
+      
+});
